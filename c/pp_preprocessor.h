9664ddaa61639b41f6913ed5b962839991d8117d
drpriver <david@davidpriver.com>
2026-01-27 02:34:07 -0800

    working on cpp

--- c/pp_preprocessor.h
+++ c/pp_preprocessor.h
@@ -5,14 +5,52 @@
 //
 
 #include "../Drp/atom_map.h"
+#include "../Drp/Allocators/allocator.h"
+#include "../Drp/long_string.h"
+#include "../Drp/file_cache.h"
+#include "../Drp/atom_table.h"
+#include "../Drp/logger.h"
+#include "../Drp/env.h"
+
+#ifndef MARRAY_STRING_VIEW
+#define MARRAY_STRING_VIEW
+#define MARRAY_T StringView
+#include "../Drp/Marray.h"
+#endif
+
 #ifdef __clang__
 #pragma clang assume_nonnull begin
 #endif
 
 typedef struct CPreprocessor CPreprocessor;
 struct CPreprocessor {
+    Allocator allocator;
     AtomMap(CMacro) macros;
-    struct 
+    FileCache* fc;
+    AtomTable* at;
+    Logger* logger;
+    Environment* env;
+    /*
+     * 1. For the quote form of the include directive, the directory of the
+     *    current file is searched first.
+     * 2. For the quote form of the include directive, the directories specified
+     *    by -iquote options are searched in left-to-right order, as they appear
+     *    on the command line.
+     * 3. Directories specified with -I options are scanned in left-to-right
+     *    order.
+     * 4. Directories specified with -isystem options are scanned in
+     *    left-to-right order.
+     * 5. Standard system directories are scanned.
+     * 6. Directories specified with -idirafter options are scanned in
+     *    left-to-right order.
+     */
+    Marray(StringView)  iquote_paths,
+                        Ipaths,
+                        isystem_paths,
+                        istandard_system_paths,
+                        idirafter_paths,
+                        framework_paths;
+
 
 };
 
--- cpp.c
+++ cpp.c
@@ -12,34 +12,110 @@
 #include "Drp/term_util.h"
 #include "Drp/file_cache.h"
 #include "c/pp_tok.h"
+#include "c/pp_preprocessor.h"
 #ifdef __clang__
 #pragma clang assume_nonnull begin
 #endif
+
+static
+int
+ma_sv_appender(ArgToParse* ap, const void* arg){
+    CPreprocessor* cpp = ap->dest.user_pointer->user_data;
+    Marray(StringView)* dst = ap->dest.pointer;
+    const StringView* sv = arg;
+    int err = ma_push(StringView)(dst, cpp->allocator, *sv);
+    return err;
+}
+
+static
+ArgParseDestination
+cpp_ma_sv_dest(const ArgParseUserDefinedType* t, Marray(StringView)*dst){
+    ArgParseDestination dest = {
+        .type = ARG_USER_DEFINED,
+        .pointer = dst,
+        .user_pointer = t,
+    };
+    return dest;
+}
+
+
+
 int main(int argc, char** argv, char** envp){
     Logger logger = std_logger();
     AtomTable at = {.allocator=MALLOCATOR};
     Environment env = {.allocator=MALLOCATOR, .at=&at};
+    FileCache* fc = fc_create(MALLOCATOR);
+
     int err = env_parse_posix(&env, envp);
     if(err){
         log_error(&logger, "Unable to parse environment");
         return 1;
     }
+    CPreprocessor cpp = {
+        .allocator = MALLOCATOR,
+        .fc = fc,
+        .at = &at,
+        .logger = &logger,
+        .env = &env,
+    };
+    ArgParseUserDefinedType t = {
+        .type_name = SV("path"),
+        .user_data = &cpp,
+    };
     const char* filename = NULL;
-    const char* extra_include_paths[32] = {0};
     ArgToParse pos_args[] = {
         {
             .name = SV("file"),
             .dest = ARGDEST(&filename),
             .help = "The file to preprocess.",
-            .min_num = 1, .max_num = 1,
+            .min_num = 0, .max_num = 1,
         }
     };
     ArgToParse kw_args[] = {
         {
             .name = SV("-I"),
-            .dest = ARGDEST(extra_include_paths),
-            .max_num = arrlen(extra_include_paths),
+            .dest = cpp_ma_sv_dest(&t, &cpp.Ipaths),
+            .append_proc = ma_sv_appender,
             .help = "Extra include paths.",
+            .max_num = 1000,
+            .one_at_a_time = 1,
+            .space_sep_is_optional = 1,
+        },
+        {
+            .name = SV("-isystem"),
+            .dest = cpp_ma_sv_dest(&t, &cpp.isystem_paths),
+            .append_proc = ma_sv_appender,
+            .help = "Extra system include paths.",
+            .max_num = 1000,
+            .one_at_a_time = 1,
+            .space_sep_is_optional = 1,
+        },
+        {
+            .name = SV("-iquote"),
+            .dest = cpp_ma_sv_dest(&t, &cpp.iquote_paths),
+            .append_proc = ma_sv_appender,
+            .help = "Extra quote include paths.",
+            .max_num = 1000,
+            .one_at_a_time = 1,
+            .space_sep_is_optional = 1,
+        },
+        {
+            .name = SV("-idirafter"),
+            .dest = cpp_ma_sv_dest(&t, &cpp.idirafter_paths),
+            .append_proc = ma_sv_appender,
+            .help = "Extra dirafter include paths.",
+            .max_num = 1000,
+            .one_at_a_time = 1,
+            .space_sep_is_optional = 1,
+        },
+        {
+            .name = SV("-F"),
+            .dest = cpp_ma_sv_dest(&t, &cpp.framework_paths),
+            .append_proc = ma_sv_appender,
+            .help = "Extra framework paths.",
+            .max_num = 1000,
+            .one_at_a_time = 1,
+            .space_sep_is_optional = 1,
         },
     };
     enum {HELP, HIDDEN_HELP, FISH};
@@ -91,31 +167,30 @@ int main(int argc, char** argv, char** envp){
         default:
             break;
     }
-    enum ArgParseError parse_err = parse_args(&parser, &args, 0);
+    enum ArgParseError parse_err = parse_args(&parser, &args, ARGPARSE_FLAGS_ALLOW_KWARG_SEP_TO_BE_OPTIONAL);
     if(parse_err){
         print_argparse_error(&parser, parse_err);
         return 1;
     }
-    FileCache* fc = fc_create(MALLOCATOR);
-    StringView this[10] = {0};
-    for(int i = 0; i < 10; i++){
-        fc_write_path(fc, __FILE__, sizeof __FILE__ - 1);
-        err = fc_read_file(fc, this+i);
-        if(err){
-            log_error(&logger, "Unable to read %s: %d (%s)", __FILE__, err, strerror(err));
-            return err;
-        }
-        if(!sv_equals(this[0], this[i])){
-            log_error(&logger, "uhh... %d", i);
-            return 1;
-        }
-        if(this[0].text != this[i].text){
-            log_error(&logger, "uhh... %d", i);
-            return 1;
-        }
+    MARRAY_FOR_EACH(StringView, p, cpp.iquote_paths){
+        log_printf(&logger, "-iquote %zd) %.*s", p-cpp.iquote_paths.data, (int)p->length, p->text);
     }
-    // log_printf(&logger, "%.*s", (int)this[0].length, this[0].text);
-    fc_destroy(fc);
+    MARRAY_FOR_EACH(StringView, p, cpp.Ipaths){
+        log_printf(&logger, "-I %zd) %.*s", p-cpp.Ipaths.data, (int)p->length, p->text);
+    }
+    MARRAY_FOR_EACH(StringView, p, cpp.isystem_paths){
+        log_printf(&logger, "-isystem %zd) %.*s", p-cpp.isystem_paths.data, (int)p->length, p->text);
+    }
+    MARRAY_FOR_EACH(StringView, p, cpp.istandard_system_paths){
+        log_printf(&logger, "standard_system_paths %zd) %.*s", p-cpp.istandard_system_paths.data, (int)p->length, p->text);
+    }
+    MARRAY_FOR_EACH(StringView, p, cpp.idirafter_paths){
+        log_printf(&logger, "-idirafter %zd) %.*s", p-cpp.idirafter_paths.data, (int)p->length, p->text);
+    }
+    MARRAY_FOR_EACH(StringView, p, cpp.framework_paths){
+        log_printf(&logger, "-F %zd) %.*s", p-cpp.framework_paths.data, (int)p->length, p->text);
+    }
+
     return 0;
 }
 
