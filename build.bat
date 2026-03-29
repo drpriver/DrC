ddd8428e2d2b1400026104389102a22fe4889ac8
drpriver <david@davidpriver.com>
2026-03-28 23:25:32 -0700

    fix build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
+where cl >nul 2>nul && (cl /nologo /std:c11 /Zc:preprocessor /wd5105 build.c /Fe:build.exe && build.exe %* -b Bin) && exit /b
 where clang >nul 2>nul && (clang build.c -o build.exe && build.exe %* -b Bin) && exit /b
-where gcc >nul 2>nul && (gcc build.c -o build.exe && build.exe %* -b Bin) && exit /b
-where cl >nul 2>nul && (cl build.c /Fe:build.exe && build.exe %* -b Bin) && exit /b
 echo No C compiler found
 exit /b 1
