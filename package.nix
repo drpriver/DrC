e7879004dbcaaf2757ba0c1c4620b2a041589c5f
drpriver <david@davidpriver.com>
2026-09-07 20:07:41 -0700

    add nix integration

---
+++ package.nix
@@ -0,0 +1,74 @@
+{ stdenv, buildPackages, lib, libffi, }:
+let
+    host = stdenv.hostPlatform;
+    hostOS = if host.isDarwin then "macos"
+             else if host.isLinux then "linux"
+             else if host.isWindows then "windows"
+             else throw "Unsupported host OS: ${host.system}";
+    hostArch = if host.isx86_64 then "x86"
+               else if host.isAarch64 then "arm"
+               else throw "Unsupported host architecture: ${host.system}";
+    hostBits = if host.isx86_64 then "64bit"
+               else if host.isAarch64 then "64bit"
+               else throw "Unsupported host bitness: ${host.system}";
+in stdenv.mkDerivation (self: {
+    passthru = {
+        inherit hostOS hostArch hostBits;
+    };
+    name = "drc";
+    configurePhase = ''
+        runHook preConfigure
+        $CC_FOR_BUILD build.c -o build -DDEFAULT_BUILD_COMPILER="\"$CC_FOR_BUILD\""
+        ./build nothing \
+            --builddir=bin \
+            --prefix=$out \
+            --build-cc=$CC_FOR_BUILD \
+            --host-cc=$CC \
+            --cc-handles-target \
+            --os=${hostOS} \
+            --arch=${hostArch} \
+            --bits=${hostBits} \
+            --optimize=${lib.boolToString self.optimize} \
+            --debug-symbols=${lib.boolToString self.debugInfo} \
+            --sanitize=${lib.boolToString self.debugInfo}
+        runHook postConfigure
+    '';
+    buildPhase = ''
+        runHook preBuild
+        ./build drc drcpp
+        runHook postBuild
+    '';
+    checkPhase = ''
+        runHook preCheck
+        ./build tests
+        runHook postCheck
+    '';
+    doCheck = false; # stdenv.buildPlatform.canExecute stdenv.hostPlatform;
+    installPhase = ''
+        runHook preInstall
+        ./build install
+        runHook postInstall
+    '';
+    strictDeps = true;
+    src = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+            ./build.c
+            ./Drp
+            ./C
+            ./Vendored
+            ./cc.c
+            ./cpp.c
+            ./drc_test.c
+            ./cc_repl_completion.h
+            ./cpp_args.h
+        ];
+    };
+    nativeCheckInputs = lib.optional self.doCheck buildPackages.libffi;
+    depsBuildBuild = [ buildPackages.stdenv.cc ];
+    buildInputs = [libffi];
+    optimize = true;
+    debugInfo = false;
+    dontStrip = self.debugInfo;
+    sanitize = false;
+})
---
+++ release.nix
@@ -0,0 +1,41 @@
+let pkgs = import <nixpkgs> {
+    overlays = [
+        (final: prev: {
+            drc = final.lib.makeScope final.newScope (self: {
+                drc = self.callPackage ./package.nix {};
+                devShell = final.mkShell {
+                    inputsFrom = [ self.drc ];
+                    depsBuildBuild = [ final.buildPackages.stdenv.cc ];
+                    # Allow the build helper to rebuild itself inside the checkout.
+                    NIX_ENFORCE_PURITY = 0;
+                    shellHook = 
+                        let 
+                            config = self.drc.stdenv.hostPlatform.config; 
+                        in ''
+                            export BUILD="${config}"-build
+                            (
+                                set -ex
+                                if [ ! -e "$BUILD" ]; then
+                                    "$CC_FOR_BUILD" build.c -o "$BUILD" -DDEFAULT_BUILD_COMPILER="\"$CC_FOR_BUILD\""
+                                fi
+                                ./"$BUILD" nothing \
+                                    --host-cc="$CC" \
+                                    --build-cc="$CC_FOR_BUILD" \
+                                    --builddir=${config}-builddir \
+                                    --cc-handles-target \
+                                    --os=${self.drc.hostOS} \
+                                    --arch=${self.drc.hostArch} \
+                                    --bits=${self.drc.hostBits}
+                            )
+                            alias mk=./"$BUILD"
+                        '';
+                };
+            });
+        })
+    ];
+};
+in {
+    inherit(pkgs.drc) drc devShell;
+    aarch64-linux-gnu = pkgs.pkgsCross.aarch64-multiplatform.drc;
+    x86_64-linux-gnu = pkgs.pkgsCross.gnu64.drc;
+}
